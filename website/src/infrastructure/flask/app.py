8edcc36c4dbe8b14160d9fdea4538c64caa66293
Anawat <66070217@kmitl.ac.th>
2025-10-09 21:33:02 +0700

    feat: add test function

--- website/src/infrastructure/flask/app.py
+++ website/src/infrastructure/flask/app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path="/static", static_folder="static", template_folder="templates")
 
+@app.route('/health-check')
+def healthCheckAPI():
+    return {
+        "status": "Running"
+    }
+
 @app.route('/')
 def homePage():
     return render_template("home.html")
---
+++ website/test/test_app.py
@@ -0,0 +1,23 @@
+import requests
+import threading
+import time
+from src.infrastructure.flask.app import app
+
+def start_app():
+    app.run(port=8080)
+
+
+def test_getURL():
+    server = threading.Thread(target=start_app)
+    server.daemon = True
+    server.start()
+
+    time.sleep(5)
+
+    responses = [
+        requests.get("http://localhost:8080/"),
+        requests.get("http://localhost:8080/health-check")
+    ]
+
+    for response in responses:
+        assert response.status_code == 200

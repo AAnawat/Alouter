86f12a96d0199aabd7d07e588b9b3f72612d5691
AAnawat <aunwongprachanukul@gmail.com>
2025-11-02 14:06:09 +0000

    fix: Bug rabbitMQ false startup

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -1,16 +1,24 @@
 import pika
+import time
 
 from config.rabbitConfig import config as rabbit_config
+print(rabbit_config)
 
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(
-        host=rabbit_config["host"],
-        credentials=pika.PlainCredentials(
-            username=rabbit_config["username"], password=rabbit_config["password"]
-        ),
-    )
-)
+for _ in range(10):
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=rabbit_config["host"],
+                credentials=pika.PlainCredentials(
+                    username=rabbit_config["username"],
+                    password=rabbit_config["password"]
+                )
+            )
+        )
+        break
+    except pika.exceptions.AMQPConnectionError:
+        print("RabbitMQ not ready, retrying...")
+        time.sleep(3)
 channel = connection.channel()
 
 channel.queue_declare(queue="interface_queue", durable=True)
